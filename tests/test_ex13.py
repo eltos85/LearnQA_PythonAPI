d4203f1b5806ba72c6e4ff7b819db68ab1c753a8
aa_solnyshkin <aa_solnyshkin@bingo-boom.com>
2022-07-01 15:48:25 +0300

    ex14

--- tests/test_ex13.py
+++ tests/test_ex13.py
@@ -2,15 +2,13 @@ import requests
 import json
 import pytest
 import yaml
-y = open("configs/config.yaml")
+y = open("../configs/config.yaml")
 yaml_conf = yaml.safe_load(y)
 
 @pytest.mark.parametrize("agent", [i for i in yaml_conf])
 def test_ex13(agent):
     request = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check", headers={"User-Agent": yaml_conf.get(agent).get('User-Agent')}).content
     response = json.loads(request)
-    print('\n', yaml_conf.get(agent).get('User-Agent'))
-    print(yaml_conf.get(agent).get('browser'))
     assert yaml_conf.get(agent).get('platform') in response['platform'], f"Wrong response param value 'platform': {response['platform']}"
     assert yaml_conf.get(agent).get('browser') in response['browser'], f"Wrong response param value 'browser': {response['browser']}"
     assert yaml_conf.get(agent).get('device') in response['device'], f"Wrong response param value 'platform': {response['device']}"
