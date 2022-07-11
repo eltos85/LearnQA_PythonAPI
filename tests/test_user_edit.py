afad24bc80677e1bb9c766fd9768d33781611bf9
aa_solnyshkin <aa_solnyshkin@bingo-boom.com>
2022-07-11 11:35:34 +0300

    Ex16

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,11 +1,11 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 class TestUserEditer(BaseCase):
     def test_edit_just_created_user(self):
         register_data = self.prepare_registration_data()
-        response1 = requests.post("https://playground.learnqa.ru/api/user", data=register_data)
+        response1 = MyRequests.post("api/user", data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -19,14 +19,14 @@ class TestUserEditer(BaseCase):
             'email': email,
             'password': password
             }
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        response2 = MyRequests.post("user/login", data=login_data)
 
         auth_sid = self.get_cookie(response2, 'auth_sid')
         token = self.get_header(response2, 'x-csrf-token')
 
         new_name = 'Changed Name'
 
-        response3 = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}",
+        response3 = MyRequests.put(f"user/{user_id}",
                                  headers={'x-csrf-token': token},
                                  cookies={'auth_sid': auth_sid},
                                  data={'firstName': new_name}
@@ -34,7 +34,7 @@ class TestUserEditer(BaseCase):
         Assertions.assert_code_status(response3, 200)
 
 
-        response4 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+        response4 = MyRequests.get(f"user/{user_id}",
                                  headers={'x-csrf-token': token},
                                  cookies={'auth_sid': auth_sid}
                                  )
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,5 +1,5 @@
 import pytest
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
@@ -8,14 +8,14 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("user", data=data)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_exesting_email(self):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("user", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"User with email '{email}' already exist", \
             f"Unexpected response content {response.content}"
@@ -23,7 +23,7 @@ class TestUserRegister(BaseCase):
     def test_create_user_with_wrong_email(self):
         email = 'vinkotovexample.com'
         data = self.prepare_registration_data(email)
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("user", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == "Invalid email format", \
             f"Unexpected response content {response.content}"
@@ -31,7 +31,7 @@ class TestUserRegister(BaseCase):
     @pytest.mark.parametrize('param', ['email', 'password', 'username', 'firstName', 'lastName'])
     def test_create_user_with_wrong_data(self, param):
         data = self.get_params(param)
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("user", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"The following required params are missed: {param}", \
             f"Unexpected response content {response.content}"
@@ -39,7 +39,7 @@ class TestUserRegister(BaseCase):
     @pytest.mark.parametrize('lens', [1, 250])
     def test_create_user_by_name_length(self, lens):
         data = self.get_random_name(lens)
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("user", data=data)
         if lens == 1:
             Assertions.assert_code_status(response, 400)
             assert response.content.decode("utf-8") == "The value of 'username' field is too short", \
